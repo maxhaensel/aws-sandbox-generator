dffa9647dfdeb363ec12f9d9146cb82e8fa7e500
Maximilian Hänsel <mh@maxhaensel.de>
2022-02-10 20:44:16 +0100

    disable ct

--- frontend/cypress/plugins/index.js
+++ frontend/cypress/plugins/index.js
@@ -1,6 +1,6 @@
-const injectDevServer = require('@cypress/react/plugins/react-scripts')
+// const injectDevServer = require('@cypress/react/plugins/react-scripts')
 
 module.exports = (on, config) => {
-  injectDevServer(on, config)
+  // injectDevServer(on, config)
   return config
 }
