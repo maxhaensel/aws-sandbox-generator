41329578353129c68dd76ca67c9cf136279f0b6f
Maximilian Hänsel <mh@maxhaensel.de>
2022-04-02 13:46:48 +0200

    mock errors

--- frontend/src/api/graphql/mocks/leaseSandBox.ts
+++ frontend/src/api/graphql/mocks/leaseSandBox.ts
@@ -1,29 +1,41 @@
+import { ApolloError } from '@apollo/client/errors'
+
 type input = { email: string; leaseTime: string; cloud: string }
 
 function leaseSandBox(parent: any, { email, leaseTime, cloud }: input) {
-  if (email === 'successful.mail@pexon-consulting.de') {
+  if (email === 'error.mail@pexon-consulting.de') {
+    return new ApolloError({
+      errorMessage: 'no valid Pexon-Mail',
+    })
+  }
+
+  if (email === 'aws.mail@pexon-consulting.de') {
     return {
-      message: 'Sandbox is provided',
-      sandbox: {
-        account_id: 'Sandbox 3',
-        account_name: 'String',
-        available: 'String',
-        assigned_until: 'String',
-        assigned_since: 'String',
-        assigned_to: 'String',
-      },
+      __typename: 'AwsSandbox',
+      id: 'Sandbox 3',
+      accountName: 'String',
+      assignedUntil: 'string',
+      assignedSince: 'string',
+      assignedTo: 'string',
     }
   }
-  return {
-    __typename: 'AzureSandbox',
-    id: 'this is a great id',
-    pipelineId: 'string',
-    assignedUntil: 'string',
-    assignedSince: 'string',
-    assignedTo: 'string',
-    status: 'string',
-    projectId: 'string',
-    webUrl: 'string',
+
+  if (email === 'azure.mail@pexon-consulting.de') {
+    return {
+      __typename: 'AzureSandbox',
+      id: 'this is a great id',
+      pipelineId: 'string',
+      assignedUntil: 'string',
+      assignedSince: 'string',
+      assignedTo: 'string',
+      status: 'string',
+      projectId: 'string',
+      webUrl: 'string',
+    }
   }
+
+  return new ApolloError({
+    errorMessage: 'internal Server Error',
+  })
 }
 export { leaseSandBox }
