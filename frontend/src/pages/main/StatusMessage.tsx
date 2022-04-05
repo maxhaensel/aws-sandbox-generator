dd3858ebeb813f127ee508ec8ddb59c3bd1f69af
Maximilian Hänsel <mh@maxhaensel.de>
2022-04-05 20:47:00 +0200

    add syntactic sugar

--- frontend/src/pages/main/StatusMessage.tsx
+++ frontend/src/pages/main/StatusMessage.tsx
@@ -1,7 +1,6 @@
-import React from "react"
 import { LeaseSandBox_leaseSandBox } from './__generated__/LeaseSandBox'
 
-type A = {
+type Props = {
   status: {
     successfully: boolean
     message: string
@@ -10,40 +9,39 @@ type A = {
   }
 }
 
-function StatusMessage(props: A) {
-  console.log(props.status.sandbox?.__typename)
-  const getSandboxName = () => props.status.sandbox?.__typename === "AwsSandbox" ? props.status.sandbox.accountName : props.status.sandbox?.sandboxName
+function StatusMessage({ status: { successfully, message, sandbox } }: Props) {
+  const getSandboxName = () =>
+    sandbox?.__typename === 'AwsSandbox'
+      ? sandbox.accountName
+      : sandbox?.sandboxName
   return (
     <div
       data-cy="alert"
       className={`
             mt-4 border-2 px-4 py-3 rounded relative
-            ${props.status.successfully
-          ? 'border-green-500 text-green-800'
-          : 'border-rose-500 text-rose-800'
-        }
+            ${
+              successfully
+                ? 'border-green-500 text-green-800'
+                : 'border-rose-500 text-rose-800'
+            }
             `}
       role="alert"
     >
-      {!props.status.successfully &&
+      {!successfully && (
         <>
-          <strong className="font-bold">
-            Fehler
-          </strong>{' '}
-          <span className="block sm:inline">{props.status.message}</span>
+          <strong className="font-bold">Fehler</strong>{' '}
+          <span className="block sm:inline">{message}</span>
         </>
-      }
-      {props.status.successfully &&
+      )}
+      {successfully && (
         <>
-          <strong className="font-bold">
-            Erfolgreich!
-          </strong>{' '}
+          <strong className="font-bold">Erfolgreich!</strong>{' '}
           <p>Name: {getSandboxName()}</p>
-          <p>Zugewiesen an: {props.status.sandbox?.assignedTo}</p>
-          <p>Gültig bis: {props.status.sandbox?.assignedUntil}</p>
-          <p>ID: {props.status.sandbox?.id}</p>
+          <p>Zugewiesen an: {sandbox?.assignedTo}</p>
+          <p>Gültig bis: {sandbox?.assignedUntil}</p>
+          <p>ID: {sandbox?.id}</p>
         </>
-      }
+      )}
     </div>
   )
 }
