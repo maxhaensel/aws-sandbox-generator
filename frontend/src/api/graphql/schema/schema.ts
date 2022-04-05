e9e8b5dac1174261b3e152e746cdd58a88087928
Maximilian Hänsel <mh@maxhaensel.de>
2022-04-05 20:16:30 +0200

    add local schema from file

--- frontend/src/api/graphql/schema/schema.ts
+++ frontend/src/api/graphql/schema/schema.ts
@@ -1,8 +1,8 @@
-import gql from 'graphql-tag'
 import { loader } from 'graphql.macro'
 
 const typeDefs = loader('../schema.graphql')
+const typeDefsLocal = loader('../schema_local.graphql')
 
-const schema = [typeDefs]
+const schema = [typeDefs, typeDefsLocal]
 
 export default schema
